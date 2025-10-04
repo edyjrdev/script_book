f63d1328b1edf1611b0a7602d3ee60c52857e1cc
Edy Epumuceno Rodrigues Junior <edyjrdev@gmail.com>
2025-10-04 00:55:18 -0300

    ajuste 002.sh

--- bash/002.sh
+++ bash/002.sh
@@ -6,10 +6,11 @@
 # Objetivo: Backup de Pasta com tar.gz
 
 #linux wsl
-home_dir="/home/edyjr/"
+home_dir="/home/edyjr"
 
 #backup no windows 
 backup_dir="/mnt/c/t/backup"
+
 
 mkdir -p $backup_dir
 
@@ -17,8 +18,9 @@ dia_atual=$(date '+%Y-%m-%d')
 
 echo "Backup de $home_dir no arquivo $backup_dir/$dia_atual.tar.gz" 
 
-tar czf $backup_dir/$dia_atual.tar.gz -C $home_dir .\
- 2>&1 backup.log 
+# tar czf /mnt/c/t/backup/teste.tar.gz -C /home/edyjr .
+tar czf $backup_dir/$dia_atual.tar.gz -C $home_dir .
+
 echo "Backup concluído com sucesso!"
 
 ls $backup_dir -lh
